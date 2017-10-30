223571dd834c9646dc83967996c02a91f6cefe6a
Caleb Porzio <calebporzio@gmail.com>
2017-10-30 11:06:25 -0400

    consolodate 'run file' and 'run method' command into one 'run' command

--- README.md
+++ README.md
@@ -2,16 +2,26 @@
 
 ![Demo GIF](demo.gif)
 
-## How To Run:
-- Place your cursor in the file/method you want to run
+## Running a specific test method:
+- Place your cursor in/on the method you want to run
 - Open the command menu: `cmd+shift+p`
 - Select: `Tasks: Run Task`
-- Select: `phpunit: method` to run a phpunit method
-- Select: `phpunit: file` to run a phpunit file
+- Select: `phpunit: run` to run a phpunit method
+
+## Running an entire test file:
+- Place your cursor on/above the class declaration line
+- Open the command menu: `cmd+shift+p`
+- Select: `Tasks: Run Task`
+- Select: `phpunit: run` to run a phpunit method
+
+## Run the previous test:
+- Open the command menu: `cmd+shift+p`
+- Select: `Tasks: Run Task`
+- Select: `phpunit: run previous` to run the last executed test
 
 ## Features:
 - Color output!
-- Run individual methods by placing your cursor anywhere in the method
+- Run individual methods by placing your cursor anywhere in/on the method
 - Test failures are displayed in the "Problems" panel for quick access
 
 > Note: this plugin registers "tasks" to run phpunit, not a command like other extensions. This makes it possible to leverage the problem output and color terminal.
@@ -20,19 +30,18 @@ Keybindings:
 > Note: you will have to add these bindings to your own `keybindings.json` file, otherwise, the bindings will not work right.
 ```
 {
-    "key": "cmd+k cmd+o",
+    "key": "cmd+k cmd+r",
     "command": "workbench.action.tasks.runTask",
-    "args": "phpunit: method"
+    "args": "phpunit: run"
 },
 {
-    "key": "cmd+k cmd+i",
+    "key": "cmd+k cmd+p",
     "command": "workbench.action.tasks.runTask",
-    "args": "phpunit: file"
+    "args": "phpunit: run previous"
 }
 ```
 
 ## Roadmap:
-- Running specific test suites
 - Not using tasks, but using plain commands?
 - Handling PHP fatal and parser errors
 - A sidebar panel for managing errors
--- extension.js
+++ extension.js
@@ -1,40 +1,45 @@
 const vscode = require('vscode');
 
+var previousFileName;
+var previousFilterString;
+
 function activate(context) {
-
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
     let provider = vscode.workspace.registerTaskProvider('phpunit', {
         provideTasks: () => {
             const rootDirectory = vscode.workspace.rootPath;
             const fileName = vscode.window.activeTextEditor.document.fileName;
             const methodName = getMethodName(vscode.window.activeTextEditor.selection.active.line);
 
-            return [
+            let filterString = methodName ? `--filter '/^.*::${methodName}$/'` : '';
+
+            const tasks = [
                 new vscode.Task(
-                    {type: "phpunit", task: "file"},
-                    "file",
+                    { type: "phpunit", task: "run" },
+                    "run",
                     'phpunit',
-                    new vscode.ShellExecution(`${rootDirectory}/vendor/bin/phpunit ${fileName}`),
+                    new vscode.ShellExecution(`${rootDirectory}/vendor/bin/phpunit ${fileName} ${filterString}`),
                     '$phpunit'
                 ),
                 new vscode.Task(
-                    { type: "phpunit", task: "method" },
-                    "method",
+                    { type: "phpunit", task: "run previous" },
+                    "run previous",
                     'phpunit',
-                    new vscode.ShellExecution(`${rootDirectory}/vendor/bin/phpunit ${fileName} --filter ${methodName}`),
+                    new vscode.ShellExecution(`${rootDirectory}/vendor/bin/phpunit ${previousFileName} ${previousFilterString}`),
                     '$phpunit'
                 ),
             ];
+
+            previousFileName = fileName;
+            previousFilterString = filterString;
+
+            return tasks;
         },
         resolveTask(task) {
             return undefined;
         }
     });
 
-    context.subscriptions.push([disposable, provider]);
+    context.subscriptions.push(provider);
 }
 exports.activate = activate;
 
@@ -43,15 +48,13 @@ function deactivate() {
 exports.deactivate = deactivate;
 
 function getMethodName(lineNumber) {
-    let methodName = '';
+    let methodName;
     let line = lineNumber;
 
     while (line > 0) {
         const lineText = vscode.window.activeTextEditor.document.lineAt(line).text;
-        const methodMatch = lineText.match(/^\s*(?:public|private|protected)?\s*function\s*(\w+)\s*\(.*$/);
-        const classMatch = lineText.match(/^class\s*(\w*)\s*.*$/);
-        if (methodMatch || classMatch) {
-            const match = methodMatch || classMatch;
+        const match = lineText.match(/^\s*(?:public|private|protected)?\s*function\s*(\w+)\s*\(.*$/);
+        if (match) {
             methodName = match[1];
             break;
         }
