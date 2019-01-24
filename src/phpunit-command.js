18defbb53cbbc6f4c93d55d15838c9b2adcbe586
Hank Hampel <hank@tigger.murmelbu.de>
2019-01-24 12:23:06 +0100

    Add implementation for running specified suite(s)

--- src/phpunit-command.js
+++ src/phpunit-command.js
@@ -16,8 +16,12 @@ module.exports = class PhpUnitCommand {
             return this.lastOutput;
         }
 
+        let runSuites = vscode.workspace.getConfiguration('better-phpunit').get('runSuites') || "";
+        if (runSuites) {
+            runSuites = ' --testsuite '.concat(runSuites.replace(/\s+/g,''));
+        }
         this.lastOutput = this.runFullSuite
-            ? `${this.binary}${this.suffix}`
+            ? `${this.binary}${runSuites}${this.suffix}`
             : `${this.binary} ${this.file}${this.filter}${this.configuration}${this.suffix}`;
 
         return this.lastOutput;
