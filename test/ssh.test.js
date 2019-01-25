40c2d3a89f714ef75f9b039b27920b5cb68c0ded
Hank Hampel <hank@tigger.murmelbu.de>
2019-01-25 12:19:55 +0100

    Add tests for Docker with and without using SSH

--- test/ssh.test.js
+++ test/ssh.test.js
@@ -24,6 +24,8 @@ describe("SSH Tests", function () {
         await vscode.workspace.getConfiguration("better-phpunit").update("ssh.host", "ahost");
         await vscode.workspace.getConfiguration("better-phpunit").update("ssh.port", "2222");
         await vscode.workspace.getConfiguration("better-phpunit").update("docker.enable", false);
+        await vscode.workspace.getConfiguration("better-phpunit").update("docker.command", null);
+        await vscode.workspace.getConfiguration("better-phpunit").update("docker.paths", null);
 
         const paths = {};
         paths[path.join(vscode.workspace.rootPath)] = "/some/remote/path";
@@ -40,6 +42,8 @@ describe("SSH Tests", function () {
         await vscode.workspace.getConfiguration("better-phpunit").update("ssh.binary", null);
         await vscode.workspace.getConfiguration("better-phpunit").update("ssh.paths", {});
         await vscode.workspace.getConfiguration("better-phpunit").update("docker.enable", false);
+        await vscode.workspace.getConfiguration("better-phpunit").update("docker.command", null);
+        await vscode.workspace.getConfiguration("better-phpunit").update("docker.paths", null);
     });
 
     it("Commands are not wrapped when SSH is disabled", async function () {
@@ -67,6 +71,45 @@ describe("SSH Tests", function () {
         assert.equal(
             extension.getGlobalCommandInstance().output,
             'ssh -tt -p2222 auser@ahost "/some/remote/path/vendor/bin/phpunit /some/remote/path/tests/SampleTest.php --filter \'^.*::test_first( .*)?$\'"'
+        );
+    });
+
+    it("The correct Docker command is run when triggering Better PHPUnit", async function () {
+        await vscode.workspace.getConfiguration("better-phpunit").update("ssh.enable", false);
+        await vscode.workspace.getConfiguration("better-phpunit").update("docker.enable", true);
+        await vscode.workspace.getConfiguration("better-phpunit").update("docker.command", "docker exec CONTAINER");
+        const paths = {};
+        paths[path.join(vscode.workspace.rootPath)] = "/some/remote/path";
+        paths["/some/other_local/path"] = "/some/other_remote/path";
+        await vscode.workspace.getConfiguration("better-phpunit").update("docker.paths", paths);
+        let document = await vscode.workspace.openTextDocument(path.join(vscode.workspace.rootPath, 'tests', 'SampleTest.php'));
+        await vscode.window.showTextDocument(document, { selection: new vscode.Range(7, 0, 7, 0) });
+        await vscode.commands.executeCommand('better-phpunit.run');
+
+        await timeout(waitToAssertInSeconds, () => { })
+
+        assert.equal(
+            extension.getGlobalCommandInstance().output,
+            'docker exec CONTAINER /some/remote/path/vendor/bin/phpunit /some/remote/path/tests/SampleTest.php --filter \'^.*::test_first( .*)?$\''
+        );
+    });
+
+    it("The correct Docker command is run via SSH when triggering Better PHPUnit", async function () {
+        await vscode.workspace.getConfiguration("better-phpunit").update("docker.enable", true);
+        await vscode.workspace.getConfiguration("better-phpunit").update("docker.command", "docker exec CONTAINER");
+        const paths = {};
+        paths[path.join(vscode.workspace.rootPath)] = "/some/remote/path";
+        paths["/some/other_local/path"] = "/some/other_remote/path";
+        await vscode.workspace.getConfiguration("better-phpunit").update("docker.paths", paths);
+        let document = await vscode.workspace.openTextDocument(path.join(vscode.workspace.rootPath, 'tests', 'SampleTest.php'));
+        await vscode.window.showTextDocument(document, { selection: new vscode.Range(7, 0, 7, 0) });
+        await vscode.commands.executeCommand('better-phpunit.run');
+
+        await timeout(waitToAssertInSeconds, () => { })
+
+        assert.equal(
+            extension.getGlobalCommandInstance().output,
+            'ssh -tt -p2222 auser@ahost "docker exec CONTAINER /some/remote/path/vendor/bin/phpunit /some/remote/path/tests/SampleTest.php --filter \'^.*::test_first( .*)?$\'"'
         );
     });
 
